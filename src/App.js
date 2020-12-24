eaa274b37b75973e600cc2d1a03bade5b6e58388
AM-77 <amine.griche77@protonmail.ch>
2020-12-24 18:25:47 +0100

    add router, header, nav ...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import useDarkMode from './hooks/useDarkMode';
+import { lightTheme, darkTheme } from './theme';
+import GlobalStyle from './theme/GlobalStyle';
+import AppWrapper from './components/AppWrapper/index';
+import TopMenu from './containers/TopMenu';
+import Header from './containers/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Work from './pages/Work';
+import WorkDetails from './pages/WorkDetails';
+import Contact from './pages/Contact';
 
-function App() {
+const App = () => {
+  const [mode, setMode] = useDarkMode();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={mode === 'light' ? lightTheme : darkTheme}>
+      <GlobalStyle />
+      <Router>
+        <TopMenu
+          isMenuOpen={isMenuOpen}
+          closeMenu={() => setIsMenuOpen(false)}
+        />
+        <AppWrapper isMenuOpen={isMenuOpen}>
+          <Header
+            setTheme={setMode}
+            onOpenMenu={setIsMenuOpen}
+            isMenuOpen={isMenuOpen}
+          />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/work/details/:workId" component={WorkDetails} />
+            <Route path="/work" component={Work} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </AppWrapper>
+      </Router>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
